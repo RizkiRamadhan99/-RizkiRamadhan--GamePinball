3e4f9f310c1955099536cd7b88cd2d8766d517e8
RizkiRamadhan99 <rizkirmdn@gmail.com>
2023-08-27 21:00:59 +0700

    Push Full Project

--- Assets/Scripts/Gameplay/ScoreUIManager.cs
+++ Assets/Scripts/Gameplay/ScoreUIManager.cs
@@ -5,17 +5,12 @@ using TMPro;
 
 public class ScoreUIManager : MonoBehaviour
 {
-	// reference ke text score nya
-	// disini menggunakan TMP_Text karena yang dipakai adalah TextMeshPro
-	// Jangan salah ya, yang nantinya dimasukan ke sini adalah text angka, bukan title nya
 	public TMP_Text scoreText;
 
-	// reference ke score manager
 	public ScoreManager scoreManager;
 
 	private void Update()
 	{
-		// agar lebih mudah, tiap update kita set aja angak score text nya menjadi angka score
 		scoreText.text = scoreManager.score.ToString();
 	}
 }
--- Assets/Scripts/UI/GameOverUIController.cs
+++ Assets/Scripts/UI/GameOverUIController.cs
@@ -5,18 +5,15 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class GameOverUIController : MonoBehaviour
 {
-	// reference untuk button
 	public Button mainMenuButton;
 
 	private void Start()
 	{
-		// setup event saat button di klik
 		mainMenuButton.onClick.AddListener(MainMenu);
 	}
 
 	public void MainMenu()
 	{
-		// kembali ke main menu
 		SceneManager.LoadScene("MainMenu");
 	}
 }
